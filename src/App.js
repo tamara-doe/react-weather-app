21e3e76f0648b71d9580d55e8a70a40924152fb4
tamara-doe <116146682+tamara-doe@users.noreply.github.com>
2023-01-02 13:05:33 +0100

    added github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Tamara Dörflinger and is
+        <a
+          href="https://github.com/tamara-doe/react-weather-app"
+          target="_blank"
+        >
+          open sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
