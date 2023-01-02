04a33e0d1019f95e9eb120984d9fb149080bf5d1
tamara-doe <116146682+tamara-doe@users.noreply.github.com>
2023-01-02 13:37:23 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Tamara Dörflinger and is
-        <a
-          href="https://github.com/tamara-doe/react-weather-app"
-          target="_blank"
-        >
-          open sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Tamara Dörflinger and is
+          <a
+            href="https://github.com/tamara-doe/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
